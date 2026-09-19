8c5d7e6b3015d5f69fae86d107e408583cd79485
Shoyan <shoyanalis@gmail.com>
2026-09-20 00:11:47 +0530

    Fix Turso Prisma integration

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,27 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const libsql = createClient({
+  url: process.env.DATABASE_TURSO_DATABASE_URL!,
+  authToken: process.env.DATABASE_TURSO_AUTH_TOKEN!,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
